2ec2addf77b3d752cdb42acaa548b5876db2d489
Benjamin Bollen <ben@doru.io>
2024-05-25 16:22:54 +0200

    (pop): footer and layout

---
+++ pop-app/src/lib/components/Footer.svelte
@@ -0,0 +1,11 @@
+<script lang=ts">
+    import { goto } from '$app/navigation';
+</script>
+
+<nav>
+    <ul>
+        <li><button on:click={() => goto('/messaging')}>Chat</button></li>
+        <li><button on:click={() => goto('/community')}>Community</button></li>
+        <li><button on:click={() => goto('/wallet')}>Wallet</button></li>
+    </ul>
+</nav>
