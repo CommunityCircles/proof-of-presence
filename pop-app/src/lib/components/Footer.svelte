63cc832d1c8d21e80d51784f662fdb5a50cbc5e7
Benjamin Bollen <ben@doru.io>
2024-05-25 16:40:54 +0200

    (footer): add style

--- pop-app/src/lib/components/Footer.svelte
+++ pop-app/src/lib/components/Footer.svelte
@@ -9,3 +9,31 @@
         <li><button on:click={() => goto('/wallet')}>Wallet</button></li>
     </ul>
 </nav>
+
+<style>
+    nav {
+        background-color: #333;
+        color: white;
+        padding: 1 rem;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+    ul {
+        list-style: none;
+        display: flex;
+        justify-content: space-around;
+        padding: 0;
+        margin: 0;
+    }
+    li {
+        margin: 0 1rem;
+    }
+    button {
+        background: none;
+        border: none;
+        color: white;
+        font-size: 1rem;
+        cursor: pointer;
+    }
+</style>
